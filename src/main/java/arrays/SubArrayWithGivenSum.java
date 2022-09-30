b8ac1916b56aee84e6f902caaf84f2ec12d28a48
Shuhratjon <shrayimjonov@uzkassa.uz>
2022-09-30 16:33:39 +0500

    Exact Solution

--- src/main/java/arrays/SubArrayWithGivenSum.java
+++ src/main/java/arrays/SubArrayWithGivenSum.java
@@ -1,88 +1,90 @@
 package arrays;
 
-import jdk.management.resource.internal.inst.FileOutputStreamRMHooks;
-
-import java.sql.Array;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class SubArrayWithGivenSum {
 
     public static void main(String[] args) {
-//       int [] a= {1, 2, 3, 7, 5};
-
-        LocalDateTime date=LocalDateTime.now();
-        System.out.println(date);
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(subarraySum_1_3(a, 10, 15));
     }
-    static ArrayList<Integer> subarraySum(int[] arr, int n, int s)
-    {
-        int sum=0;
-        int k=0;
-        int counter=0;
-        ArrayList<Integer> list=new ArrayList<>();
-//        for(int i=k;i<n;i++){
-////            for(int j=i;j<n;j++){
-////                sum+=arr[j];
-////                if(sum==s){
-////                    list.add(i+1) ;list.add(j+1);
-////                    return list;
-////                }
-////                k++;
-////            }
-////            sum-=arr[c];
-////            c++;
-////            i=k;
-////            for (int integer : arr) {
-////                sum+=integer;
-////                if (sum==s){
-////                    list.add(0,)
-////                }
-////            }
-//
-//
-//        }
-//        list.add(0,-1);
-//        return list;
-        for (int i=counter;i<n;i++){
-            for (int j=i;j  <n;j++){
 
-                sum+=arr[j];
-                if(sum==s){
-                    list.add(0,k+1);
-                     list.add(1,j+1);
-                     return list;
+    static ArrayList<Integer> subarraySum_1(int[] arr, int n, int s) {
+        int sum = 0;
+        int k = 0;
+        int counter = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = counter; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                sum += arr[j];
+                if (sum == s) {
+                    list.add(0, k + 1);
+                    list.add(1, j + 1);
+                    return list;
                 }
                 counter++;
-                if(sum>s){
+                if (sum > s) {
                     break;
                 }
             }
-            sum-=arr[k];
+            sum -= arr[k];
             k++;
-            i=counter;
+            i = counter;
 
         }
-        list.add(0,-1);
+        list.add(0, -1);
         return list;
 
     }
 
-    public static ArrayList<Integer> subarraySum_1(int [] arr,int n,int s){
-        int sum=0;
-        int k=1;
-        ArrayList<Integer>list=new ArrayList<>();
+    public static ArrayList<Integer> subarraySum_1_2(int[] arr, int n, int s) {
+        int sum = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum < s && i == n - 1 || sum > s && i == n - 1) {
+                list.add(0, -1);
+                return list;
+            }
+            if (sum > s) {
+                sum -= arr[n - i];
+            }
+            if (sum == s) {
+                list.add(0, n - i + 1);
+                list.add(1, i + 1);
+                return list;
+            }
 
+        }
         return list;
     }
-    public static void findSum(int[]arr,int sum,int s,int k,ArrayList<Integer> list){
-        for (int i=k;i<arr.length;i++){
-if (sum==s){
-    list.add(0,k);
-    list.add(1,i);
-}
+
+    public static ArrayList<Integer> subarraySum_1_3(int[] arr, int n, int s) {
+        int k = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == s) {
+                list.add(0, k + 1);
+                list.add(1, i + 1);
+                return list;
+            }
+            if (sum > s) {
+                sum = 0;
+                k++;
+                i = k - 1;
+            }
+
+            if ((sum < s || sum > s) && i == n - 1) {
+                list.add(0, -1);
+                return list;
+            }
         }
+        list.add(0, -1);
+        return list;
+
     }
 }
